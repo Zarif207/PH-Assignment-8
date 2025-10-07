e8f35908656ce4bd0e2e3327fb08b926ce50d477
Zarif207 <zarifhasan207@gmail.com>
2025-10-07 19:20:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-2xl bg-yellow-500'>Hello Assignment 8</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Error/ErrorPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>ERROR</h1>
+            <h3>Page Not Found</h3>
+        </div>
+    );
+};
+
+export default ErrorPage;   
